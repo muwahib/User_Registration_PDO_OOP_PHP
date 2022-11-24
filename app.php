30d810a797a832dddb2bb1e856d9f76b3141827a
Muwahib Abdirahman <muwahib2050@gmail.com>
2022-11-24 12:24:01 -0800

    1st commit

---
+++ app.php
@@ -0,0 +1,61 @@
+<?php
+
+
+
+?>
+
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <title>REGISTRATION FORM</title>
+</head>
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+
+<body>
+    <div class="main-content">
+        <div class="container mt-7">
+            <!-- Table -->
+            <h2 class="mb-5">USER TABLE</h2>
+            <div class="row">
+
+                <div class="col">
+                    <div class="card shadow">
+                        <div class="card-header border-0">
+                            <h3 class="mb-0">User Information</h3>
+                        </div>
+                        <div class="table-responsive">
+                            <table class="table align-items-center table-flush">
+                                <thead class="thead-light">
+                                    <tr>
+                                        <th scope="col">ID</th>
+                                        <th scope="col">NAME</th>
+                                        <th scope="col">LASTNAME</th>
+                                        <th scope="col">Email</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Ahmed</td>
+                                        <td>Ismail</td>
+                                        <td>Ahmed@email.com</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="footer">
+        <div class="row align-items-center justify-content-xl-between">
+            <div class="col-xl-6 m-auto text-center">
+                <div class="copyright">
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+
+
+?>
+
+
+<html lang="en">
+<head>
+<link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <title>REGISTRATION FORM</title>
+</head>
+<body>
+  
+<div class='bold-line'></div>
+<div class='container'>
+  <div class='window'>
+    <div class='overlay'></div>
+    <div class='content'>
+      <div class='welcome'>Hello There!</div>
+      <div class='subtitle'>We're almost done. Before using our services you need to create an account.</div>
+      <div class='input-fields'>
+        <input type='text' placeholder='Username' class='input-line full-width'></input>
+        <input type='email' placeholder='Email' class='input-line full-width'></input>
+        <input type='password' placeholder='Password' class='input-line full-width'></input>
+
+      </div>
+      <div class='spacing'>or continue with <span class='highlight'>Facebook</span></div>
+      <div><button class='ghost-round full-width'>Create Account</button></div>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
